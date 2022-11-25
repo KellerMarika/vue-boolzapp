524e6ccd503f46963ade972a7bcbb81743f478b1
KellerMarika <marikakeller92@gmail.com>
2022-11-25 16:46:01 +0100

    extractTimeFromDate

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
- import usersList from "./usersList.js"
+import usersList from "./usersList.js"
 
 const { createApp } = Vue
 
@@ -11,10 +11,33 @@ createApp({
 
     },
     methods: {
+        /* FUNZIONE RECUPERO ORA DA STRINGA data + ora
+           trasforma stringa in array (spazio)
+           rimuovi il primo dato(splice(0,1))?
+           recupera quel dato come variabile
+           rimuovi ultimi 2 caratteri dall'unico elemento
+       */
 
+        extractTimeFromDate(string) {
+            console.log(string)
+
+            let dateArray = string.split(" ")
+            //console.log(dateArray)
+            dateArray = dateArray.pop().split("")
+            //console.log(dateArray)
+            dateArray.length = 5
+            //console.log(dateArray)
+            const date = dateArray.join("")
+            //console.log(date)
+            return date
+
+
+        }
     },
 
     mounted() {
 
     },
 }).mount("#app")
+
+
